0ac34d9a5afeaad4814b1a4fbb22bada5953848c
ilyasshalimi <81758595+ilyasshalimi@users.noreply.github.com>
2024-12-02 12:46:35 +0100

    Update variables.tf

--- ecr/variables.tf
+++ ecr/variables.tf
@@ -1,6 +1,6 @@
 variable "docker_image_tag" {
   type        = string
-  description = "This is the tag which will be used for the image that you created"
+  description = ""
   default     = "latest"
 }
 
